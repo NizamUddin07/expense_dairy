48d1b1577df73f9295a5d543624fe2159b7d79f5
Md Nizam Uddin <nizzamuddin71@gmail.com>
2025-05-09 20:18:28 +0600

    project started

---
+++ lib/expenses.dart
@@ -0,0 +1,38 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense>_registeredExpense = [
+    Expense(
+      title : 'flutter course',
+      amount: 20,
+      date: DateTime.now(),
+      category: Category.work
+    ),
+    Expense(
+        title : 'Cinema',
+        amount: 10,
+        date: DateTime.now(),
+        category: Category.leisure
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Hello text"),
+          Expanded(child: ExpensesList(expenses: _registeredExpense))
+        ],
+      ),
+    );
+  }
+}
